34605ac5204e9bc17ef935b151d6e1b6aa3aa340
Jose Gonzalez <coding.tamalito@gmail.com>
2024-04-24 16:28:50 +0200

    add page to join poarty

---
+++ src/pages/party/JoinParty.js
@@ -0,0 +1,29 @@
+import {useParams} from "react-router-dom";
+
+export default function JoinParty() {
+    // get the party_id
+    const params = useParams();
+    const partyId = params.id;
+
+    const header = <h1>Would you like to join party: {partyId}</h1>
+    const joinButton = <button onClick={() => {joinParty(partyId)}}>Join!</button>
+} // end of componenet
+
+
+/**
+ * Sends the get request to join the party
+ * @param partyId
+ */
+function joinParty(partyId) {
+
+    fetch(`http://localhost:8080/joinParty/${partyId}`, {
+        method: 'GET',
+        credentials: 'include'
+    }).then(res => {
+        console.log(`Response Code: ${res.status}`);
+        // get the json response
+        res.json().then(data => {
+            console.log(data)
+        })
+    }).catch(err => console.error(err));
+} // end of join party
