cce915d65198eb80c27cb3dc52671c41529e5bd3
Jose Gonzalez <coding.tamalito@gmail.com>
2024-04-25 17:38:22 +0200

    modify logic for a successful request

--- src/pages/party/JoinParty.js
+++ src/pages/party/JoinParty.js
@@ -1,13 +1,14 @@
 import {useParams} from "react-router-dom";
 
 export default function JoinParty() {
+
     // get the party_id
     const params = useParams();
     const partyId = params.id;
 
     const header = <h1>Would you like to join party: {partyId}</h1>
     const joinButton = <button onClick={() => {joinParty(partyId)}}>Join!</button>
-
+    const requested = <h2>Access to the party has been requested, wait for the owner to let you in</h2>
     return (
         <>
             {header}
@@ -28,6 +29,10 @@ function joinParty(partyId) {
         credentials: 'include'
     }).then(res => {
         console.log(`Response Code: ${res.status}`);
+        if (res.status === 200) {
+            // everything went alright, user was added to the queue to join the party
+            window.location.href = '/'
+        }
         // get the json response
         res.json().then(data => {
             console.log(data)
